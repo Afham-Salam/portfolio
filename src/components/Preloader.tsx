950cd27d468cf154734530a76b39b7690076b7e3
Afham salam <afhamsalam8060@gmail.com>
2025-03-13 12:55:14 +0530

    changed

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { gsap } from "gsap";
 import { OrbitControls } from "three-stdlib";
 
