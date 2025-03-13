1124caebe78e7dfedb9caff5b10783eebf6760c8
Afham salam <afhamsalam8060@gmail.com>
2025-03-13 12:44:38 +0530

    Modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -18,4 +19,4 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000, // Increase chunk warning limit
   },
-});
+})
